e9e73f2d33e7266a617e995c0bf6feb9c554b088
Ahana Shetty <ahanashetty8@gmail.com>
2026-06-28 22:53:38 +0530

    about update

--- components/WhyUs.tsx
+++ components/WhyUs.tsx
@@ -37,6 +37,8 @@ export default function WhyUs() {
           moments with sparkle.
         </h2>
 
+        <div className="h-4 md:h-6" />
+
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
 
           {stats.map((item) => (
